f7d86e6aa6b27b248c14f3a9f9f719e05c303115
tiennt <tokentien@gmail.com>
2024-08-20 06:05:08 +0700

    Initial commit

---
+++ src/Http/Controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SocolaDaiCa\TestPhpstanDisallowedCalls\Http\Controllers;
+
+class UserController
+{
+    public function index()
+    {
+        return view('user.index');
+    }
+}
---
+++ src/Http/Kernel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SocolaDaiCa\TestPhpstanDisallowedCalls\Http;
+
+class Kernel extends \SocolaDaiCa\LaravelBadassium\Contracts\Http\Kernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * This middleware is run during every request to your application.
+     *
+     * @var array<int, class-string|string>
+     */
+    protected array $middleware = [
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array<string, array<int, class-string|string>>
+     */
+    protected array $middlewareGroups = [
+    ];
+
+    /**
+     * The application's route middleware groups extend.
+     *
+     * @var array<string, array<int, class-string|string>>
+     */
+    protected array $extendMiddlewareGroups = [
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * This middleware may be assigned to group or used individually.
+     *
+     * @var array<string, class-string|string>
+     */
+    protected array $middlewareAliases = [
+    ];
+}
